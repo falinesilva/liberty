d5ccdb461bb8cee661fe0f366a0fb7a89e83d318
falinedev <faline.custodio.da.silva@gmail.com>
2025-05-09 15:01:55 -0300

    Feat: Added summary and button to close it.

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
-console.log("Hello world!");
-
-const btn = document.querySelector(".btn-add-item");
+const add_item_btn = document.querySelector(".btn-add-item");
 const form = document.querySelector(".item-form");
 
-btn.addEventListener("click", function () {
+add_item_btn.addEventListener("click", function () {
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
-    btn.textContent = "Close";
+    add_item_btn.textContent = "Close";
   } else {
     form.classList.add("hidden");
-    btn.textContent = "Add item";
+    add_item_btn.textContent = "Add item";
   }
+});
+
+// SUMMARY
+let totalLiabilities = document.querySelector(".total-liabilities");
+let totalAssets = document.querySelector(".total-assets");
+let netWorth = document.querySelector(".net-worth");
+
+let liabilities = parseFloat(totalLiabilities.textContent) || 0;
+let assets = parseFloat(totalAssets.textContent) || 0;
+
+let result = assets - liabilities;
+
+netWorth.textContent = result;
+
+const close_summary_btn = document.querySelector(".btn-close-summary");
+
+const summary = document.querySelector(".summary");
+
+const summary_btn = document.querySelector(".btn-summary");
+
+const items = document.querySelector(".items");
+
+summary_btn.addEventListener("click", function () {
+  if (summary.classList.contains("hidden")) {
+    summary.classList.remove("hidden");
+    items.classList.add("hidden");
+  }
+  close_summary_btn.addEventListener("click", function () {
+    if (!summary.classList.contains("hidden")) {
+      summary.classList.add("hidden");
+      items.classList.remove("hidden");
+    }
+  });
 });
