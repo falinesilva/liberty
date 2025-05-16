16dce9ca2b25f6f739cbc9e5ff91994e6a7c1e19
falinedev <faline.custodio.da.silva@gmail.com>
2025-05-16 17:28:44 -0300

    Supabse integration started.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Selecting DOM elements
 const menuBtn = document.querySelector(".btn-menu");
 const menu = document.querySelector(".menu");
 const form = document.querySelector(".item-form");
@@ -6,6 +7,54 @@ const stats = document.querySelector(".net-worth");
 const menuImg = document.querySelector(".menu-img");
 const saveBtn = document.querySelector(".btn-add-save");
 const cancelBtn = document.querySelector(".btn-add-cancel");
+const itemList = document.querySelector(".item-list");
+
+//Create DOM elements: Render items in list
+itemList.innerHTML = "";
+
+// Load data from Supabase
+loadItems();
+
+async function loadItems() {
+  const res = await fetch(
+    "https://bagvahadiehddzgbibhj.supabase.co/rest/v1/items",
+    {
+      headers: {
+        apikey:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJhZ3ZhaGFkaWVoZGR6Z2JpYmhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4NDE2MTMsImV4cCI6MjA2MTQxNzYxM30.-mlmD04uoAd7YAChxX2QKFU_z6iznkS7lUTAodJb_0s",
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJhZ3ZhaGFkaWVoZGR6Z2JpYmhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4NDE2MTMsImV4cCI6MjA2MTQxNzYxM30.-mlmD04uoAd7YAChxX2QKFU_z6iznkS7lUTAodJb_0s",
+      },
+    }
+  );
+
+  const data = await res.json();
+
+  console.log(data);
+  createItemList(data);
+}
+
+function createItemList(dataArray) {
+  const htmlArr = dataArray.map(
+    (item) => `
+    
+      <ul class="item-list">
+        <li class="item"><span class="name">${item.name}</span><br />
+          <span class="type">${item.type}</span><br />
+          <span class="class asset">${item.class}</span><br />
+          <span class="value">${item.value}</span>
+          <div class="item-buttons">
+            <button class="btn btn-delete-item">Delete</button>
+            <button class="btn btn-edit-item">Edit</button>
+          </div></li></ul>`
+  );
+
+  const html = htmlArr.join("");
+
+  itemList.insertAdjacentHTML("afterbegin", html);
+}
+
+// Menu Functionality
 menuBtn.addEventListener("click", function () {
   const menuHidden = menu.classList.contains("hidden");
   const formVisible = !form.classList.contains("hidden");
