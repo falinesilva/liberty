d04232448b74290db03b78c411f546d74c31d482
falinedev <faline.custodio.da.silva@gmail.com>
2025-05-31 15:08:28 -0300

    Reverting back to pre-JS implementation. I'm learning, and this is an exercise.

--- data.js
+++
@@ -1,9 +0,0 @@
-const COLOR_SCHEME = {
-  primary: "#1E2328", // Dark Gray/Black
-  secondary: "#F5B301", // Bright Yellow
-  background: "#f8fafc", // Light Gray/Off-White
-  accent: "#3B3F46", // Slate Gray
-  text: "#2A2E34", // Darker Slate Gray
-};
-
-console.log(COLOR_SCHEME);
