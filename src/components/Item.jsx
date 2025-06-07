9cd6189bedefd2bb92d1110273781ff08f80f03a
falinesilva <faline.silva@proton.me>
2025-06-07 08:19:59 -0300

    ui styling

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,17 +1,18 @@
 function Item({ item }) {
   return (
     <div className="item">
-      <div>
-        <span className="name">{item.name}</span>
-        <br />
-        <span className="type">{item.type}</span>
-        <br />
-        <span className="value">{item.value}</span>
-      </div>
       <div className="item-buttons">
         <button className="btn btn-delete-item">Delete</button>
         <button className="btn btn-edit-item">Edit</button>
       </div>
+      <div>
+        <span className="name">{item.name.toUpperCase()}</span>
+        <br />
+      </div>
+      <div>
+        <span className="type">{item.type.toUpperCase()}</span>
+      </div>
+      <span className="value">$ {item.value.toLocaleString()}</span>
     </div>
   );
 }
