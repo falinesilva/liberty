e07fc4260fa1e5e4b3d6107ebd86650fe97af096
unknown <faline.silva@proton.me>
2025-06-26 09:08:15 -0300

    feat: added svg logo to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
 import supabase from "../supabase";
 
+import { ReactComponent as Logo } from "../assets/Logo.svg";
+
 async function signOut() {
   await supabase.auth.signOut();
 }
 
-function Header({ user, displayName }) {
+function Header({ user }) {
   return (
     <>
       <div className="flex justify-between items-center">
-        <div>{displayName}</div>
+        <Logo className="h-10 w-auto fill-current text-slate-50" />
+
+        <div>{user.email}</div>
 
         <button className="btn-primary" onClick={signOut}>
           {user ? "Sign Out" : "Log in"}
